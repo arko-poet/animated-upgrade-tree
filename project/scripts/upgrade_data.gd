474853fde52c7b82733be53e76e7b5d55bc2c695
arko <arkadiusz.kupracz@gmail.com>
2026-09-25 16:45:10 +0100

    add upgrade data and node placement

--- project/scripts/upgrade_data.gd
+++ project/scripts/upgrade_data.gd
@@ -3,4 +3,7 @@ extends Resource
 
 @export var id: int
 @export var icon: Texture2D
-@export var name: String
+@export var name: String = "Name"
+@export var cost: int = 1
+@export var max_upgrades: int = 3
+@export var unlocks: Array[int]
