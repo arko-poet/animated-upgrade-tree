11502b334cc1a007ed56d8b23fc2613c5a92aad5
arko <arkadiusz.kupracz@gmail.com>
2026-09-25 16:05:23 +0100

    add upgrade tree node

--- project/scenes/upgrade_node.gd
+++ project/scenes/upgrade_node.gd
@@ -1,6 +1,10 @@
 class_name UpgradeNode
 extends Control
 
+const _MAX_COLOR := Color(0.957, 0.89, 0.467, 1.0)
+const _AFFORDABLE_COLOR := Color(0.424, 0.949, 0.502, 1.0)
+const _UNAFFORDABLE_COLOR := Color(0.835, 0.278, 0.322, 1.0)
+
 @export var upgrade_data: UpgradeData
 
 # Called when the node enters the scene tree for the first time.
--- project/scenes/upgrade_tree.gd
+++ project/scenes/upgrade_tree.gd
@@ -2,18 +2,26 @@ class_name UpgradeTree
 extends Node
 
 
-var _is_dragging := false
+var _is_dragging := false:
+	set(value):
+		_is_dragging = value
+		set_process(_is_dragging)
+		_last_mouse_position = get_viewport().get_mouse_position()
 var _last_mouse_position: Vector2
 
-@onready var camera: Camera2D = %Camera
+@onready var _camera: Camera2D = %Camera
+
+
+func _ready() -> void:
+	set_process(false)
 
 
 func _process(_delta: float) -> void:
-	_is_dragging =  Input.is_action_pressed("drag")
-	
-	if not _is_dragging:
-		_last_mouse_position = get_viewport().get_mouse_position()
-	else:
-		var next_mouse_position = get_viewport().get_mouse_position()
-		camera.position -= next_mouse_position - _last_mouse_position
-		_last_mouse_position = next_mouse_position
+	var next_mouse_position = get_viewport().get_mouse_position()
+	_camera.position += _last_mouse_position - next_mouse_position
+	_last_mouse_position = next_mouse_position
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action("drag"):
+		_is_dragging = event.is_pressed()
